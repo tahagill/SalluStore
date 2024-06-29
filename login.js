275153fabac2a4c812c835199f4b21d8f2c340df
Umut Aykan Koç <52351484+DrH0pe@users.noreply.github.com>
2024-06-29 16:46:35 +0300

    MA-9/Update-Login-Page-JS

--- login.js
+++ login.js
@@ -1,35 +1,79 @@
 //Log-in Page Start - UMUT
-document.addEventListener('DOMContentLoaded', function () {
-    const loginButton = document.querySelector('.loginPageFormButton input[type="button"]');
-    const errorMessage = document.querySelector('.loginPageFormErrorMessage');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
 
-    loginButton.addEventListener('click', function () {
-        const username = usernameInput.value;
-        const password = passwordInput.value;
+const users = [
+  {
+      id: 1,
+      username: 'admin',
+      password: 'admin'
+  },
+  {
+      id: 2,
+      username: 'alperen',
+      password: '12345678'
+  },
+  {
+      id: 3,
+      username: 'umut',
+      password: '12345678'
+  },
+  {
+      id: 4,
+      username: 'erkin',
+      password: '12345678'
+  },
+  {
+      id: 5,
+      username: 'esra',
+      password: '12345678'
+  },
+  {
+      id: 6,
+      username: 'deniz',
+      password: '12345678'
+  },
+  {
+      id: 7,
+      username: 'merve',
+      password: '12345678'
+  }
+];
 
-        if (username === 'admin' && password === 'admin') {
-            window.location.href = 'index.html';
-        } else {
-            if (username === '' || password === '') {
-                errorMessage.textContent = 'Please enter Email / Phone Number and Password.';
-            } else {
-                errorMessage.textContent = 'Email / Phone Number or Password is incorrect. Please try again.';
-            }
-        }
-        loginButton.disabled = true;
+document.addEventListener('DOMContentLoaded', function() {
+// Select the login button
+const loginButton = document.querySelector('.loginPageFormButton input[type="button"]');
+const errorMessage = document.querySelector('.loginPageFormErrorMessage');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
 
-        setTimeout(function () {
-            loginButton.disabled = false;
-        }, 3000);
-    });
-    usernameInput.addEventListener('input', function () {
-        loginButton.disabled = false;
-    });
+  loginButton.disabled = true;
 
-    passwordInput.addEventListener('input', function () {
-        loginButton.disabled = false;
-    });
+loginButton.addEventListener('click', function() {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    window.location.href = 'index.html';
+  } else {
+    if (username === '' || password === '') {
+      errorMessage.textContent = 'Please enter Email / Phone Number and Password.';
+    } else {
+      errorMessage.textContent = 'Email / Phone Number or Password is incorrect. Please try again.';
+    }
+  }
+  loginButton.disabled = true;
+
+  setTimeout(function() {
+    loginButton.disabled = false;
+  }, 1000);
+});
+usernameInput.addEventListener('input', function() {
+  loginButton.disabled = false;
+});
+
+passwordInput.addEventListener('input', function() {
+  loginButton.disabled = false;
+});
 });
 //Log-in Page End - UMUT
