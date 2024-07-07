0b51109116b647d20e57260b22899b65778c5bca
Macbook <macbook@192.168.1.74>
2024-07-07 19:27:50 +0300

    [MA-4]-Homepage-Best-Selling-Products contain javascript

--- app.js
+++ app.js
@@ -19,3 +19,29 @@ calculateDiscount(firstOldPrice.textContent, firstDisPrice);
 calculateDiscount(secondOldPrice.textContent, secondDisPrice);
 
 calculateDiscount(thirdOldPrice.textContent, thirdDisPrice);
+let allProducts=[]
+const bestproducts = document.querySelectorAll("#bestProductsContainer");
+async function getProducts(){
+  try{
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    allProducts= data;
+    getBestSellingProducts();
+  } catch (error){
+    console.error("Hata oluştu.", error);
+  }
+}
+getProducts();
+function getBestSellingProducts(){
+    const firstProducts = allProducts.slice(0,4);
+    bestproducts.innerHTML = firstProducts.map((product)=>{
+      return `<div class="best-products">
+              <img class="best-products-img" src="${product.image}" alt="${product.title}">
+              <h3 class="best-products-title">${product.title}</h3>
+              <div class="best-product-prices-container"><p class="best-product-price-discounted">$${(product.price*0.3).toFixed(2)}</p>
+              <p class="best-product-price">$${product.price}</p></div>
+              <span class="sale-price">%30</span>
+        </div>`;
+    })
+    .join("");
+}
