1a43e05af155a47ab02b1a56be3832e6e457a582
Macbook <macbook@192.168.1.74>
2024-07-07 20:49:41 +0300

    [MA-4]-Homepage-Best-Selling-Products version2

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-//calculate product price discount
-
-const firstDisPrice = document.querySelector(".first-discounted-price");
-const firstOldPrice = document.querySelector(".first-preprice");
-
-const secondDisPrice = document.querySelector(".second-discounted-price");
-const secondOldPrice = document.querySelector(".second-preprice");
-
-const thirdDisPrice = document.querySelector(".third-discounted-price");
-const thirdOldPrice = document.querySelector(".third-preprice");
-
-function calculateDiscount(oldPrice, newPrice) {
-  const discountAmount = oldPrice * 0.3;
-  const discountedPrice = oldPrice - discountAmount;
-  newPrice.textContent = discountedPrice;
+const CategoryBoxes = document.querySelectorAll(".category-box");
+for (let i = 0; i < CategoryBoxes.length; i++) {
+  CategoryBoxes[i].addEventListener("click", () => {
+    for (let j = 0; j < CategoryBoxes.length; j++) {
+      CategoryBoxes[j].style.backgroundColor = "#FFFFFF";
+    }
+    CategoryBoxes[i].style.backgroundColor = "#FF0000";
+  });
 }
-calculateDiscount(firstOldPrice.textContent, firstDisPrice);
-
-calculateDiscount(secondOldPrice.textContent, secondDisPrice);
-
-calculateDiscount(thirdOldPrice.textContent, thirdDisPrice);
 let allProducts=[]
-const bestproducts = document.querySelectorAll("#bestProductsContainer");
+const bestproducts = document.querySelector("#bestProductsContainer");
 async function getProducts(){
   try{
     const response = await fetch("https://fakestoreapi.com/products");
@@ -36,12 +24,25 @@ function getBestSellingProducts(){
     const firstProducts = allProducts.slice(0,4);
     bestproducts.innerHTML = firstProducts.map((product)=>{
       return `<div class="best-products">
-              <img class="best-products-img" src="${product.image}" alt="${product.title}">
-              <h3 class="best-products-title">${product.title}</h3>
-              <div class="best-product-prices-container"><p class="best-product-price-discounted">$${(product.price*0.3).toFixed(2)}</p>
-              <p class="best-product-price">$${product.price}</p></div>
-              <span class="sale-price">%30</span>
-        </div>`;
+                <img class="best-products-img" src="${product.image}" alt="${product.title}">
+                <h3 class="best-products-title">${product.title}</h3>
+                <div class="best-product-prices-container">
+                  <p class="best-product-price-discounted">$${(product.price*0.3).toFixed(2)}</p>
+                  <s class="best-product-price">$${product.price}</s>
+                </div>
+                <div class="best-products-rate">
+                  <p class="stars">${getStars(product.rating.rate)}</p>
+                  <p class="best-product-rate">(${product.rating.count})</p>
+                </div>
+                <span class="sale-price">%30</span>
+              </div>`;
     })
     .join("");
+}
+function getStars(rating) {
+  let stars = ``;
+  for (let i = 0; i < rating.toFixed(0); i++) {
+    stars += `<img src="images/star.png" />`;
+  }
+  return stars;
 }
