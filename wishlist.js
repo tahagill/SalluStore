52155cec0affbce1a873df0f4afd9a6eab2e0429
erkinkadriaksoy <erkinkadriaksoy@gmail.com>
2024-07-14 00:59:50 +0300

    [MA-10] - added rendering of products in the wishlist.

--- wishlist.js
+++ wishlist.js
@@ -4,15 +4,37 @@ wpWishlistContainer.classList.add("wpProductContainer");
 const wpWishlistProducts =
   JSON.parse(localStorage.getItem("wishListProduct")) || [];
 
-console.log(wpWishlistProducts);
+let wpCurrentIndex = 0;
+let wpEndIndex = 4;
+
+const wpNextButton = document.getElementById("wpForward");
+const wpPrevButton = document.getElementById("wpBackward");
+
+function goBackward() {
+  if (wpCurrentIndex > 0) {
+    wpCurrentIndex -= 4;
+    wpEndIndex -= 4;
+    wpRenderWishlist();
+  }
+}
+
+function goForward() {
+  if (wpEndIndex < wpWishlistProducts.length) {
+    wpCurrentIndex += 4;
+    wpEndIndex += 4;
+    wpRenderWishlist();
+  }
+}
 
 function wpRenderWishlist() {
   wpWishlistContainer.innerHTML = "";
 
   if (wpWishlistProducts && wpWishlistProducts.length > 0) {
-    wpWishlistProducts.forEach((wpProduct) => {
-      const wpProductElement = document.createElement("div");
-      wpProductElement.innerHTML = `<div class="wpProduct">
+    wpWishlistProducts
+      .slice(wpCurrentIndex, wpEndIndex)
+      .forEach((wpProduct) => {
+        const wpProductElement = document.createElement("div");
+        wpProductElement.innerHTML = `<div class="wpProduct">
         <img src="${wpProduct.image}"/>
         <div class="wp-btn-container">
             <button>Add To Cart</button>
@@ -21,8 +43,8 @@ function wpRenderWishlist() {
         <h4>${wpProduct.title}</h4>
         <p class="wpPrice">$${wpProduct.price}</p>
        </div>`;
-      wpWishlistContainer.appendChild(wpProductElement);
-    });
+        wpWishlistContainer.appendChild(wpProductElement);
+      });
   } else {
     wpWishlistContainer.innerHTML = "İstek listenizde ürün yok";
   }
