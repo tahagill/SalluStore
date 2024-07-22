c805dea9b07093c0220b5530ea8c81bc201c6c88
erkinkadriaksoy <erkinkadriaksoy@gmail.com>
2024-07-22 18:26:18 +0300

    [MA-5] - added width/height to image

--- app.js
+++ app.js
@@ -347,21 +347,22 @@ function wpUpdateCountSeconds() {
   wpRemainingSeconds.innerHTML = `${seconds}`;
 }
 
-async function fetchRandomProductImage() {
+async function hfpRandomProductImage() {
   try {
     let response = await fetch("https://fakestoreapi.com/products");
     let products = await response.json();
 
-    let randomProduct = products[Math.floor(Math.random() * products.length)];
+    let hfpRandomProduct =
+      products[Math.floor(Math.random() * products.length)];
 
-    let productImage = randomProduct.image;
+    let hfpProductImage = hfpRandomProduct.image;
 
-    let imageBox = document.getElementById("hfpRandomImage");
-    imageBox.innerHTML = `<img class="hfp-image" src="${productImage}" alt="Random Product Image" />`;
+    let hfpImageBox = document.getElementById("hfpRandomImage");
+    hfpImageBox.innerHTML = `<img class="hfp-image" src="${hfpProductImage}" alt="Random Product Image" />`;
   } catch (error) {
     console.error("Ürün çekme hatası:", error);
   }
 }
 
-window.onload = fetchRandomProductImage;
+window.onload = hfpRandomProductImage;
 // Erkin Homepage Featured Product END
